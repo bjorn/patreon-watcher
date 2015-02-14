5635be942db2ecbd1d173dc71e2fcf89644153d0
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2015-02-14 17:00:19 +0100

    First version

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM centos:centos6
+
+# Enable EPEL for Node.js
+RUN     rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+# Install Node.js and npm
+RUN     yum install -y npm
+
+COPY src /src
+
+EXPOSE  80
+CMD ["node", "/src/index.js"]
---
+++ src/index.js
@@ -0,0 +1,57 @@
+var http = require("http");
+var https = require("https");
+
+var status = {
+    earnings: 0,
+    patrons: 0,
+};
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(status));
+}).listen(80);
+
+console.log('Server running at http://127.0.0.1:80/');
+
+function refreshStatus() {
+    var options = {
+        hostname: "www.patreon.com",
+        path: "/bjorn",
+        rejectUnauthorized: false,
+        agent: false,
+    };
+
+    var req = https.request(options, function(res) {
+        var body = ""
+
+        res.on("data", function(chunk) { body += chunk; });
+
+        res.on("end", function() {
+            var re = /var totalEarnings[^0-9]*([0-9]+\.[0-9]+)\);/
+            var result = re.exec(body);
+            if (result !== null) {
+                status.earnings = parseFloat(result[1]);
+            } else {
+                console.log("total earnings not found");
+            }
+
+            re = /\$\('#totalPatrons'\)\.text\(([0-9]+)\)/
+            result = re.exec(body);
+            if (result !== null) {
+                status.patrons = parseFloat(result[1]);
+            } else {
+                console.log("total patrons not found");
+            }
+        });
+    });
+
+    req.on("error", function(e) {
+        console.log("problem with request: " + e.message);
+    });
+
+    req.end();
+}
+
+// refresh status every 5 minutes (288 times a day)
+setInterval(refreshStatus, 1000 * 60 * 5);
+refreshStatus();
